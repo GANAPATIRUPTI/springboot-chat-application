e045a36ae78f4d3db01edcc1c7facfbec2dbab2c
GANAPATIRUPTI <ganeshdige77@gmail.com>
2026-01-02 23:34:31 +0530

    Initial commit - Spring Boot Chat Application

---
+++ src/main/java/com/chatapp/Batch09j17122025ChatApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch09j17122025ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch09j17122025ChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/Batch09j17122025ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch09j17122025ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
